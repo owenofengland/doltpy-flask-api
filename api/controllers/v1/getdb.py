34b5d8e97e348b580023762a714981b0c2943efd
owenofengland <joe215@nyu.edu>
2021-03-09 16:02:47 -0500

    ADD initial implementation of branch support

--- api/controllers/v1/getdb.py
+++ api/controllers/v1/getdb.py
@@ -2,25 +2,25 @@ from doltpy.cli.read import read_pandas_sql
 from api.helpers.getrepo import getRepo
 import pandas as pd
 
-def getBaseTables(repo):
+def getBaseTables(repo: str):
     query = '''SELECT * FROM information_schema.tables WHERE table_type = "BASE_TABLE"'''
     repo = getRepo(repo)
     result = read_pandas_sql(repo, query)
     return result.to_json()
 
-def getSystemTables(repo):
+def getSystemTables(repo: str):
     query = '''SELECT * FROM information_schema.tables WHERE table_type = "SYSTEM VIEW"'''
     repo = getRepo(repo)
     result = read_pandas_sql(repo, query)
     return result.to_json()
 
-def getAllTables(repo):
+def getAllTables(repo: str):
     query = '''SELECT * FROM information_schema.tables'''
     repo = getRepo(repo)
     result = read_pandas_sql(repo, query)
     return result.to_json()
 
-def getDataFromQuery(repo, query):
+def getDataFromQuery(repo: str, query: str):
     if query.split(' ')[0] == 'SELECT':
         repo = getRepo(repo)
         result = read_pandas_sql(repo, query)
--- api/helpers/getrepo.py
+++ api/helpers/getrepo.py
@@ -9,3 +9,20 @@ def getRepo(repoName: str):
     else:
         dolt = Dolt.clone(repoName)
     return dolt
+
+# This function and branchSupportGetRepo are the beginnings of a way to run queries on specific commits and branches
+def updateData(dolt: Dolt, branchName: str):
+    dolt.checkout(branchName)
+    dolt.pull()
+    return repo
+
+def branchSupportGetRepo(repoName: str, branchName: str):
+    if repoName.split('/')[1] in listdir('./'):
+        localRepoName = './' + repoName.split('/')[1]
+        dolt = Dolt(localRepoName)
+    else:
+        dolt = Dolt.clone(repoName)
+    dolt = updateData(dolt, branchName)
+    return dolt
+        
+        
